fdf4bafdb2929643e2030f666fc7e09efcdff0fe
saldisucipto <saldisucipto@gmail.com>
2023-07-18 17:33:04 +0700

    Slider Done

--- public/assets/js/atresnaScript.js
+++ public/assets/js/atresnaScript.js
@@ -1,21 +1,43 @@
-var slider = document.getElementById("slider");
-var sliderItem = slider.getElementsByTagName("div");
-var dots = document.getElementById("dots");
-var dotsChild = document.getElementById("dots").getElementsByTagName("li");
-for (i = 0; i < sliderItem.length; i++) {
-    dots.appendChild(document.createElement("li"));
-    dotsChild[i].classList.add("list-inline-item");
-    dotsChild[i].setAttribute("id", i);
-    dotsChild[i].innerHTML = i;
-    dotsChild[0].classList.add("active");
-    dotsChild[i].addEventListener("click", runSlider);
-}
-function runSlider() {
-    var dnum = this.getAttribute("id");
-    for (i = 0; i < sliderItem.length; i++) {
-        sliderItem[i].classList.remove("active");
-        sliderItem[dnum].classList.add("active");
-        dotsChild[i].classList.remove("active");
-        dotsChild[dnum].classList.add("active");
+// Slider Function
+let currentSlide = 1;
+
+let slideElement = document.getElementById("slider");
+let totalSlide = slideElement.childElementCount;
+
+function next() {
+    if (currentSlide < totalSlide) {
+        currentSlide++;
+        showSlide();
+    } else {
+        currentSlide = 1;
+        showSlide();
     }
 }
+
+function prev() {
+    console.log("kurentSlide =" + currentSlide);
+    console.log("totalSlide =" + totalSlide);
+
+    if (currentSlide <= 1) {
+        currentSlide = totalSlide;
+        showSlide();
+    } else {
+        currentSlide--;
+        showSlide();
+    }
+}
+
+function showSlide() {
+    console.log("show slide function");
+    slides = document.getElementById("slider").getElementsByTagName("li");
+    for (let index = 0; index < totalSlide; index++) {
+        const element = slides[index];
+        if (currentSlide === index + 1) {
+            element.classList.remove("hidden");
+            element.classList.add("show");
+        } else {
+            element.classList.add("hidden");
+        }
+    }
+}
+// End Slider Function
--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -1,12 +1,24 @@
-<div class="      ">
-
-    <div class="container">
-        <div id="slider" class="slider relative">
+<div class="mx-auto ">
+    <div class="relative drop-shadow-lg">
+        <ul id="slider" class="bg-gray-200">
             @foreach ($sliders as $item)
-                <div class="slider-item active"><img src="{{ '/storage/img/sliders/' . $item->slider_image }}"
-                        alt="" class="img-fluid"></div>
+                <li class="h-[50vh] @if (!$loop->first) hidden @endif">
+                    <img class="h-full w-full object-cover" src="{{ '/storage/img/sliders/' . $item->slider_image }}"
+                        alt="" />
+                </li>
             @endforeach
-            <ul id="dots" class="list-inline dots  text-center"></ul>
+        </ul>
+        <div class="absolute flex top-0 left-0 h-full w-full">
+            <div class="my-auto flex justify-between w-full px-2">
+                <button onclick="prev()"
+                    class="bg-white py-2 px-3 rounded-full bg-opacity-30 hover:bg-opacity-70 shadow-lg">
+                    <i class="fas fa-arrow-left"></i>
+                </button>
+                <button onclick="next()"
+                    class="bg-white py-2 px-3 rounded-full bg-opacity-30 hover:bg-opacity-70 shadow-lg">
+                    <i class="fas fa-arrow-right"></i>
+                </button>
+            </div>
         </div>
     </div>
 </div>
