593a16517103cb5a5b479eaa2dacdc40ce713c97
saldisucipto <saldisucipto@gmail.com>
2023-11-15 22:11:05 +0700

    Just Update WIth ngrok

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -8,8 +8,6 @@ use App\Http\Controllers\Data\KonfigurasiDataController;
 use App\Http\Controllers\Data\MasterDataController;
 use App\Models\BlogNews;
 use Inertia\Inertia;
-use App\Models\Produk;
-use App\Models\KategoriProduk;
 use App\Models\BrandProduk;
 use App\Models\CompanyInfo;
 use App\Models\Customer;
@@ -50,28 +48,28 @@ class DashboardController extends Controller
     }
 
     // master brand produk
-    public function brandProduk()
-    {
-        return Inertia::render('Dashboard/Master/Produk/Brand/Index', ['produk' => MasterDataController::ambilSemuaData(new BrandProduk()), 'message' => null]);
-    }
+    // public function brandProduk()
+    // {
+    //     return Inertia::render('Dashboard/Master/Produk/Brand/Index', ['produk' => MasterDataController::ambilSemuaData(new BrandProduk()), 'message' => null]);
+    // }
 
     // master servis
-    public function masterServis()
-    {
-        return Inertia::render('Dashboard/Master/Servis/Index', ['servis' => MasterDataController::ambilSemuaData(new Servis()), 'message' => null]);
-    }
+    // public function masterServis()
+    // {
+    //     return Inertia::render('Dashboard/Master/Servis/Index', ['servis' => MasterDataController::ambilSemuaData(new Servis()), 'message' => null]);
+    // }
 
     // company
-    public function masterCompany()
-    {
-        return Inertia::render('Dashboard/Master/Company/Index', ['servis' => MasterDataController::ambilSemuaData(new Servis()), 'message' => null]);
-    }
+    // public function masterCompany()
+    // {
+    //     return Inertia::render('Dashboard/Master/Company/Index', ['servis' => MasterDataController::ambilSemuaData(new Servis()), 'message' => null]);
+    // }
 
     // customer
-    public function masterCustomer()
-    {
-        return Inertia::render('Dashboard/Master/Customer/Index', ['customer' => MasterDataController::ambilSemuaData(new Customer()), 'message' => null]);
-    }
+    // public function masterCustomer()
+    // {
+    //     return Inertia::render('Dashboard/Master/Customer/Index', ['customer' => MasterDataController::ambilSemuaData(new Customer()), 'message' => null]);
+    // }
 
     // konfigurasi pages
     public function konfigurasiPages()
@@ -86,22 +84,22 @@ class DashboardController extends Controller
     }
 
     // static pages
-    public function staticPages()
-    {
-        return Inertia::render('Dashboard/Pages/Index', ['data' => MasterDataController::ambilSemuaData(new StaticPages()), 'message' => null]);
-    }
+    // public function staticPages()
+    // {
+    //     return Inertia::render('Dashboard/Pages/Index', ['data' => MasterDataController::ambilSemuaData(new StaticPages()), 'message' => null]);
+    // }
 
     // blog and news
-    public function blogPages()
-    {
-        return Inertia::render('Dashboard/BlogNews/Index', ['data' => MasterDataController::ambilSemuaData(new BlogNews()), 'message' => null]);
-    }
+    // public function blogPages()
+    // {
+    //     return Inertia::render('Dashboard/BlogNews/Index', ['data' => MasterDataController::ambilSemuaData(new BlogNews()), 'message' => null]);
+    // }
 
     // sosmed
-    public function sosmedPages()
-    {
-        return Inertia::render('Dashboard/Master/Sosmed/Index', ['data' => MasterDataController::ambilSemuaData(new Sosmed()), 'message' => null]);
-    }
+    // public function sosmedPages()
+    // {
+    //     return Inertia::render('Dashboard/Master/Sosmed/Index', ['data' => MasterDataController::ambilSemuaData(new Sosmed()), 'message' => null]);
+    // }
 
     // profile
     public function profilePages()
@@ -110,8 +108,8 @@ class DashboardController extends Controller
     }
 
     // slider
-    public function sliderPages()
-    {
-        return Inertia::render('Dashboard/Master/Slider/Index', ['data' => MasterDataController::ambilSemuaData(new Slider()), 'message' => null]);
-    }
+    // public function sliderPages()
+    // {
+    //     return Inertia::render('Dashboard/Master/Slider/Index', ['data' => MasterDataController::ambilSemuaData(new Slider()), 'message' => null]);
+    // }
 }
